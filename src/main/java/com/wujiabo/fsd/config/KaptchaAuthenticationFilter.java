c527618ff04f5e36b5fee83d966c7dab6dc14daf
Jiabo.Wu <wujbow@cn.ibm.com>
2019-08-01 00:29:34 +0800

    init

---
+++ src/main/java/com/wujiabo/fsd/config/KaptchaAuthenticationFilter.java
@@ -0,0 +1,57 @@
+package com.wujiabo.fsd.config;
+
+import com.google.code.kaptcha.Constants;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class KaptchaAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+    private String usernameParameter = "username";
+    private String passwordParameter = "password";
+    private String servletPath;
+
+    protected KaptchaAuthenticationFilter(String servletPath) {
+        super(servletPath);
+        this.servletPath = servletPath;
+        setAuthenticationFailureHandler(new SimpleUrlAuthenticationFailureHandler(servletPath));
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        if ("POST".equalsIgnoreCase(req.getMethod()) && servletPath.equals(req.getServletPath())) {
+            String expect = (String) req.getSession().getAttribute("CHECK_CODE");
+            if (expect != null && !expect.equalsIgnoreCase(req.getParameter("kaptcha"))) {
+                unsuccessfulAuthentication(req, res, new InsufficientAuthenticationException("kaptcha is incorrect"));
+                return;
+            }
+        }
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        return null;
+    }
+
+    protected String obtainPassword(HttpServletRequest request) {
+        return request.getParameter(this.passwordParameter);
+    }
+
+    protected String obtainUsername(HttpServletRequest request) {
+        return request.getParameter(this.usernameParameter);
+    }
+}
--- src/main/java/com/wujiabo/fsd/config/WebSecurityConfig.java
+++ src/main/java/com/wujiabo/fsd/config/WebSecurityConfig.java
@@ -12,13 +12,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.addFilterBefore(new KaptchaAuthenticationFilter("/login"), UsernamePasswordAuthenticationFilter.class)
             .authorizeRequests()
                 .antMatchers("/", "/register","/captcha.jpg").permitAll()
                 .anyRequest().authenticated()
