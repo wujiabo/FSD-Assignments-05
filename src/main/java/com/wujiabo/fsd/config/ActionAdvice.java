ce871261a61806d90e02a4e8205c04d3443de2fc
Jiabo.Wu <wujbow@cn.ibm.com>
2019-07-31 20:23:54 +0800

    init

--- src/main/java/com/wujiabo/fsd/config/ActionAdvice.java
+++ src/main/java/com/wujiabo/fsd/config/ActionAdvice.java
@@ -7,12 +7,15 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class ActionAdvice {
-    @ResponseBody
     @ExceptionHandler(BindException.class)
-    public String  bindExceptionHandler(BindException ex) {
-        return "field is empty";
+    public ModelAndView bindExceptionHandler(BindException ex) {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("error");
+        mv.addObject("msg", "field is empty");
+        return mv;
     }
 }
